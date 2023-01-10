c30acd18a88192d3d512a2461fc654af322c7f46
hainguyenhuy2002 <81941497+hainguyenhuy2002@users.noreply.github.com>
2023-01-10 23:20:30 +0700

    final

--- src/hash.py
+++ src/hash.py
@@ -1,104 +1,6 @@
-def translate(message):
-    charcodes = [ord(c) for c in message]
-    bytes = []
-    for char in charcodes:
-        bytes.append(bin(char)[2:].zfill(8))
-    bits = []
-    for byte in bytes:
-        for bit in byte:
-            bits.append(int(bit))
-    return bits
-
-def chunker(bits, chunk_length=8):
-    chunked = []
-    for b in range(0, len(bits), chunk_length):
-        chunked.append(bits[b:b+chunk_length])
-    return chunked
-
-def fillZeros(bits, length=8, endian='LE'):
-    l = len(bits)
-    if endian == 'LE':
-        for i in range(l, length):
-            bits.append(0)
-    else: 
-        while l < length:
-            bits.insert(0, 0)
-            l = len(bits)
-    return bits
-
-def preprocessMessage(message):
-    bits = translate(message)
-    length = len(bits)
-    message_len = [int(b) for b in bin(length)[2:].zfill(64)]
-    if length < 448:
-        bits.append(1)
-        bits = fillZeros(bits, 448, 'LE')
-        bits = bits + message_len
-        return [bits]
-    elif 448 <= length <= 512:
-        bits.append(1)
-        bits = fillZeros(bits, 1024, 'LE')
-        bits[-64:] = message_len
-        return chunker(bits, 512)
-    else:
-        bits.append(1)
-        while (len(bits)+64) % 512 != 0:
-            bits.append(0)
-        bits = bits + message_len
-        return chunker(bits, 512)
-
-def initializer(values):
-    binaries = [bin(int(v, 16))[2:] for v in values]
-    words = []
-    for binary in binaries:
-        word = []
-        for b in binary:
-            word.append(int(b))
-        words.append(fillZeros(word, 32, 'BE'))
-    return words
-
-def binToHexa(k):
-    value = ''.join([str(x) for x in k])
-    # convert binary to int
-    num = int(value, 2)
-    # convert int to hexadecimal
-    hex_num = format(num, 'x')
-    return(hex_num)
-
-
-
-def isTrue(x): return x == 1
-
-def if_(i, y, z): return y if isTrue(i) else z
-
-def and_(i, j): return if_(i, j, 0)
-def AND(i, j): return [and_(ia, ja) for ia, ja in zip(i,j)] 
-
-def not_(i): return if_(i, 0, 1)
-def NOT(i): return [not_(x) for x in i]
-
-def xor(i, j): return if_(i, not_(j), j)
-def XOR(i, j): return [xor(ia, ja) for ia, ja in zip(i, j)]
-
-def xorxor(i, j, l): return xor(i, xor(j, l))
-def XORXOR(i, j, l): return [xorxor(ia, ja, la) for ia, ja, la, in zip(i, j, l)]
-
-def maj(i,j,k): return max([i,j,], key=[i,j,k].count)
-
-def rotr(x, n): return x[-n:] + x[:-n]
-
-def shr(x, n): return n * [0] + x[:-n]
-
-def add(i, j):
-  length = len(i)
-  sums = list(range(length))
-  c = 0
-  for x in range(length-1,-1,-1):
-    sums[x] = xorxor(i[x], j[x], c)
-    c = maj(i[x], j[x], c)
-  return sums
-
-
+from utils.backup import *
+from utils.parameters import *
+from utils.process import *
 
 def sha256(message): 
     k = initializer(K)
@@ -109,8 +11,8 @@ def sha256(message):
         for _ in range(48):
             w.append(32 * [0])
         for i in range(16, 64):
-            s0 = XORXOR(rotr(w[i-15], 7), rotr(w[i-15], 18), shr(w[i-15], 3) ) 
-            s1 = XORXOR(rotr(w[i-2], 17), rotr(w[i-2], 19), shr(w[i-2], 10))
+            s0 = sigma(rotr(w[i-15], 7), rotr(w[i-15], 18), shr(w[i-15], 3) ) 
+            s1 = sigma(rotr(w[i-2], 17), rotr(w[i-2], 19), shr(w[i-2], 10))
             w[i] = add(add(add(w[i-16], s0), w[i-7]), s1)
         a = h0
         b = h1
@@ -121,11 +23,11 @@ def sha256(message):
         g = h6
         h = h7
         for j in range(64):
-            S1 = XORXOR(rotr(e, 6), rotr(e, 11), rotr(e, 25) )
+            S1 = sigma(rotr(e, 6), rotr(e, 11), rotr(e, 25) )
             ch = XOR(AND(e, f), AND(NOT(e), g))
             temp1 = add(add(add(add(h, S1), ch), k[j]), w[j])
-            S0 = XORXOR(rotr(a, 2), rotr(a, 13), rotr(a, 22))
-            m = XORXOR(AND(a, b), AND(a, c), AND(b, c))
+            S0 = sigma(rotr(a, 2), rotr(a, 13), rotr(a, 22))
+            m = sigma(AND(a, b), AND(a, c), AND(b, c))
             temp2 = add(S0, m)
             h = g
             g = f
